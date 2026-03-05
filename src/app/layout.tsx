2613f17ce800d72315b041d77e8c3251ce81f84e
yasu <buta29don@gmail.com>
2026-03-05 18:05:03 +0900

    ブラウザのタブに表示するアイコンを競輪のアイコンに変更
    - public/icon.svgに自転車のアイコンを作成
    - layout.tsxのmetadataにiconプロパティを追加してfaviconを指定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import Header from "./components/Header";
 export const metadata: Metadata = {
   title: "競輪予想システム",
   description: "競輪レースのデータ閲覧・予想支援システム",
+  icons: {
+    icon: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
